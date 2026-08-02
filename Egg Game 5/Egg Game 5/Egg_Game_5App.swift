0c897a0f4eb7a9c09636b9db3a6b04e4432c7e0a
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:14:19 -0400

    Add Egg Game 5/Egg Game 5/Egg_Game_5App.swift

---
+++ Egg Game 5/Egg Game 5/Egg_Game_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Egg_Game_5App.swift
+//  Egg Game 5
+//
+//  Created by Elliot Williams on 2025-06-23.
+//
+
+import SwiftUI
+
+@main
+struct Egg_Game_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
