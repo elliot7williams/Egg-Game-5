e7f0c7482837824b494d6e39276fff9e77a074b9
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:14:22 -0400

    Add Egg Game 5/Egg Game 5UITests/Egg_Game_5UITestsLaunchTests.swift

---
+++ Egg Game 5/Egg Game 5UITests/Egg_Game_5UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Egg_Game_5UITestsLaunchTests.swift
+//  Egg Game 5UITests
+//
+//  Created by Elliot Williams on 2025-06-23.
+//
+
+import XCTest
+
+final class Egg_Game_5UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
